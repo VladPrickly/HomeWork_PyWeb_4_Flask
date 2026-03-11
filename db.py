8672a7a0213ad38bcd3236fcc772c48acbf36f44
Vladislav <vladislav.goltsev@gmail.com>
2026-03-11 21:48:15 +0300

    create db.py

--- db.py
+++ db.py
@@ -1,6 +1,10 @@
+import atexit
+import datetime
 import os
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, MappedColumn, mapped_column
+from sqlalchemy import create_engine, Integer, Text, String, DateTime, Column, func
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_USER")
@@ -8,4 +12,23 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST=os.getenv("POSTGRES_HOST")
 POSTGRES_PORT=os.getenv("POSTGRES_PORT")
 
-engine = create_engine()
+PG_DSN = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_engine(PG_DSN)
+Session = sessionmaker(bind=engine)
+Base = declarative_base(bind=engine)
+
+
+class Advertisement(Base):
+    __tablename__ = 'advertisements'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False, index=True)
+    description = Column(Text)
+    created_at = Column(DateTime, server_default=func.now())
+    author = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+atexit.register(engine.dispose)
+
